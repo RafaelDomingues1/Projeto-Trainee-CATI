a2e73eda3871315c6bd33c08b4dd8388f4b408be
Rafael Domingues <rafaelddomingues78@gmail.com>
2026-06-28 15:50:43 -0300

    Feat : Adicionando dependências do Spring boot

---
+++ trainee2026-cati/Backend/MatriculaFacil/src/main/java/com/CATI/MatriculaFacil/MatriculaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.CATI.MatriculaFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaFacilApplication.class, args);
+	}
+
+}
---
+++ trainee2026-cati/Backend/MatriculaFacil/src/test/java/com/CATI/MatriculaFacil/MatriculaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CATI.MatriculaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
