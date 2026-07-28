209d440c665d2b29d881bc40f097367f4d2c90ad
Rafael Domingues <rafaelddomingues78@gmail.com>
2026-07-28 20:33:08 -0300

    feat : integração cadastro back e frontend

--- trainee2026-cati/frontend-example/src/components/SignupCard.tsx
+++ trainee2026-cati/frontend-example/src/components/SignupCard.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { FormEvent,useState } from 'react'
 import { GraduationCapIcon, EyeOffIcon } from '../assets/icons'
 import InputField from './InputField'
 import { Page } from '../types'
+import {cadastrarAluno} from "../services/AlunoServices";
 
 interface SignupCardProps {
   onNavigate?: (page: Page) => void
@@ -10,8 +11,78 @@ interface SignupCardProps {
 export default function SignupCard({ onNavigate }: SignupCardProps) {
   const [showPassword, setShowPassword] = useState(false)
 
+  //dados do formulário:
+  const[name,setName] = useState('')
+  const[email,setEmail] = useState('')
+  const[password,setPassword] = useState('')
+  const[confirmPassword,setConfirmPassword] = useState('')
+  const[error,setError] = useState('')
+  const[success,setSuccess] = useState('')
+  const[loading,setLoading] = useState(false) //impede vários cliques enquanto cadastra
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>){
+
+    event.preventDefault() //impede o navegador de recarregar a página
+
+    setError('') //limpa o erro antigo
+    setSuccess('') //limpa o sucesso antigo
+
+    if (
+        !name ||
+        !email ||
+        !password ||
+        !confirmPassword){
+
+      setError('Preencha todos os campos')
+      return
+    }
+
+      if(password.length< 10){
+        setError('A senha deve possuir pelo menos 10 caracteres') //backend pede isso
+        return
+      }
+
+      if(password !== confirmPassword){
+        setError('As senhas não coincidem')
+        return
+      }
+
+      try{
+
+        setLoading(true)
+
+        await cadastrarAluno({name,email,password})
+
+        setSuccess('Usuário cadastrado com sucesso!!')
+
+        setName('')
+        setEmail('')
+        setPassword('')
+        setConfirmPassword('') //limpa os campos
+
+        //Espera para o usuário ver a mensagem
+        setTimeout(() => {
+          onNavigate?.('login')
+        }, 300)
+
+
+    } catch(error){
+
+        console.error('Erro ao cadastrar usuário:',error)
+
+        setError('Não foi possível criar a conta,tente novamente.')
+
+      } finally {
+
+        setLoading(false)
+
+      }
+  }
+
+
   return (
     <div className="bg-white border border-[rgba(199,196,216,0.4)] rounded-2xl shadow-[0px_25px_50px_-12px_rgba(79,70,229,0.05)] flex flex-col gap-8 p-6 sm:p-[41px]">
+      {/*cabeçalho*/}
       <div className="flex flex-col items-center w-full">
         <div className="mb-4">
           <div className="bg-[rgba(79,70,229,0.1)] border border-[rgba(79,70,229,0.1)] flex items-center justify-center w-14 h-14 rounded-xl shadow-sm p-px">
@@ -32,23 +103,34 @@ export default function SignupCard({ onNavigate }: SignupCardProps) {
         </p>
       </div>
 
-      <div className="flex flex-col gap-5 w-full">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-5 w-full"
+      >
+        {/*nome*/}
         <InputField
           label="Nome Completo"
           type="text"
           placeholder="Ex: João da Silva"
+          value={name}
+          onChange={(event) => setName((event.target.value))}
         />
 
+        {/*email*/}
         <InputField
           label="E-mail Institucional"
           type="email"
           placeholder="joao.silva@aluno.edu.br"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
-
+        {/*senha*/}
         <InputField
           label="Senha"
           type={showPassword ? 'text' : 'password'}
           placeholder="••••••••"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
           rightIcon={
             <button
               type="button"
@@ -59,23 +141,65 @@ export default function SignupCard({ onNavigate }: SignupCardProps) {
             </button>
           }
         />
-
+        {/*Confirmar senha*/}
         <div className="pb-2">
           <InputField
             label="Confirmar Senha"
             type="password"
             placeholder="••••••••"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
           />
         </div>
 
+        {/* Erro */}
+        {error && (
+            <div className="
+            bg-red-50
+            border
+            border-red-200
+            text-red-700
+            text-sm
+            px-4
+            py-3
+            rounded-lg
+            text-center
+  ">
+              {error}
+            </div>
+        )}
+
+        {success && (
+            <p className="
+            bg-green-50
+             border
+             border-green-200
+             text-green-700
+             text-sm
+             px-4
+             py-3
+             rounded-lg
+             text-center
+  ">
+              {success}
+            </p>
+        )}
+
         <button
           type="submit"
-          className="w-full bg-brand-accent text-white font-normal text-base leading-6 py-[14px] rounded-lg shadow-[0px_4px_6px_-1px_rgba(79,70,229,0.2),0px_2px_4px_-2px_rgba(79,70,229,0.2)] hover:bg-indigo-700 active:bg-indigo-800 transition-colors"
+          disabled={loading}
+          className="w-full bg-brand-accent text-white font-normal text-base leading-6 py-[14px] rounded-lg shadow-[0px_4px_6px_-1px_rgba(79,70,229,0.2),0px_2px_4px_-2px_rgba(79,70,229,0.2)] hover:bg-indigo-700 active:bg-indigo-800 transition-colors  disabled:opacity-50
+            disabled:cursor-not-allowed"
         >
-          Criar Conta
-        </button>
-      </div>
+          {loading
+              ? 'Criando conta...'
+              : 'Criar Conta'
+          }
 
+        </button>
+      </form>
+
+        {/*Voltar para o login*/}
       <div className="pt-2">
         <div className="border-t border-[rgba(199,196,216,0.2)] pt-6 w-full">
           <div className="flex items-center justify-center gap-1">
--- trainee2026-cati/frontend-example/src/services/AlunoServices.ts
+++ trainee2026-cati/frontend-example/src/services/AlunoServices.ts
@@ -1,8 +1,13 @@
 import api from './Api.ts'
 import {Perfil} from "../types/Perfil.ts";
+import {CadastroAluno} from "../types/CadastroAluno.ts";
 
 export async function buscarPerfil(): Promise<Perfil> {
     const response = await api.get<Perfil>('aluno/perfil') //recebe a resposta que vem de get aluno/perfil
 
     return response.data
+}
+export async function cadastrarAluno(aluno: CadastroAluno): Promise<void>{
+
+    await api.post('aluno/',aluno)
 }
